ca84be9be92320aa21508752d8f6e57d253af95f
Jack3113 <jack03113@gmail.com>
2017-02-20 03:57:33 +0100

    Add favicon (only in production)

--- server/server.js
+++ server/server.js
@@ -14,11 +14,12 @@ app.use(function(req, res, next) {
     next();
 });
 app.use('/static', express.static(__dirname + '/../client/dist/static'));
+app.use('/assets', express.static(__dirname + '/../client/src/assets'));
 app.get('/', function(req, res) {
     res.sendFile(path.normalize(__dirname + '/../client/dist/index.html'));
 });
 app.use('/api', users);
 
 app.listen(3000, function() {
-    console.log('Example app listening on port 3000!');
+    console.log('Pomodoro Dashboard is running on port 3000');
 });
